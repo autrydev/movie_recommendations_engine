c99a60da51a72ef77f30ad341dfddf5d5152576b
Scott Cornaby <scott.cornaby@ufl.edu>
2022-03-28 21:51:06 -0500

    Early experiment with using spacy to tokenize a sentence and compare plots

---
+++ spacy_experiments.py
@@ -0,0 +1,53 @@
+import sys  
+import csv
+import spacy
+
+nlp = spacy.load("en_core_web_lg")
+
+
+if(len(sys.argv) != 2):
+    print("Usage: " + sys.argv[0] + " csv_file_of_move_plots")
+
+plots = []
+
+with open(sys.argv[1], 'r', encoding='utf-8') as movie_plot_csv_file:
+    reader = csv.reader(movie_plot_csv_file)
+    next(reader, None)  # skip the headers
+    count=0
+
+    for row in reader:
+        plots.append((row[1],nlp(row[7])))
+        count = count + 1
+        if(count % 100 == 0):
+            print(count, " movies processed")
+            
+print("Processing Done")
+        
+print(plots[100])
+print(plots[-1])
+
+def simularity_printer(firstId, secondId):
+    print("Similarity of ", plots[firstId][0], " and ", plots[secondId][0], ":", plots[firstId][1].similarity(plots[secondId][1]))
+    
+simularity_printer(10, 100)
+simularity_printer(10, 250)
+simularity_printer(10, 300)
+simularity_printer(27, 72)
+simularity_printer(27, 207)
+simularity_printer(27, 270)
+
+def best_match(id):
+    best_score = 0
+    best_match = "None"
+    for plot in plots:
+        if plot == plots[id]:
+            continue
+        score = plots[id][1].similarity(plot[1])
+        if score > best_score:
+            best_score = score
+            best_match = plot[0]
+    print("Best match for ",plots[id][0]," was: ",best_match," with score of ", best_score)
+
+best_match(2000)
+best_match(2500)
+best_match(3000)
