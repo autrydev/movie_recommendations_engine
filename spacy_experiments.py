37bc2022485afe61be75eda470e9dfbdfe54524f
Stephen Smith <autrydev@gmail.com>
2022-03-29 23:08:58 -0400

    Add sample stopword removal

--- spacy_experiments.py
+++ spacy_experiments.py
@@ -22,14 +22,39 @@ with open(sys.argv[1], 'r', encoding='utf-8') as movie_plot_csv_file:
             print(count, " movies processed")
             
 print("Processing Done")
-        
+
 print(plots[100])
+
+# Take Plots[10] and Plots[100] and remove the stopwords
+# TODO: Find more efficient way to remove stopwords
+# TODO: Remove stopwords from ALL plots
+
+# Create blank token list
+first_token_list = []
+# Tokenize plots - add each word to list
+for token in plots[10][1]:
+    first_token_list.append(token.text)
+
+# Create a new string combining all the tokens (words) that are not stopwords
+first_filtered_plot_str = ' '.join([str(token) for token in first_token_list if not nlp.vocab[token].is_stop])
+
+# Repeat stopword removal for second plot
+second_token_list = []
+for token in plots[100][1]:
+    second_token_list.append(token.text)      
+
+second_filtered_plot_str = ' '.join([str(token) for token in second_token_list if not nlp.vocab[token].is_stop])
+
 print(plots[-1])
 
 def simularity_printer(firstId, secondId):
     print("Similarity of ", plots[firstId][0], " and ", plots[secondId][0], ":", plots[firstId][1].similarity(plots[secondId][1]))
     
 simularity_printer(10, 100)
+
+# Print Filtered Stopword similarity to test stopword removal impact
+print("Filtered Stopwords: ", nlp(first_filtered_plot_str).similarity(nlp(second_filtered_plot_str)))
+
 simularity_printer(10, 250)
 simularity_printer(10, 300)
 simularity_printer(27, 72)
